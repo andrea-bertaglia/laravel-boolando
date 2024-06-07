184a5b200736b7e22279e26b7c2f76e1a599dc29
andrea-bertaglia <andreabertaglia@gmail.com>
2024-06-07 18:46:12 +0200

    feat: header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,32 @@
-<header class="bg-warning">
-    HEADER
+<header class="ms_bg-header">
+    <div class="container">
+        <div class="row text-white">
+            
+            <!-- menù links -->
+            <div class="col fw-bold d-flex align-items-center">
+                <ul class="d-flex align-items-center mb-0 list-unstyled gap-3">
+                    <li>Donna</li>
+                    <li>Uomo</li>
+                    <li>Bambini</li>
+                </ul>
+            </div>
+            <!-- /menù links -->
+
+            <!-- logo -->
+            <div class="col d-flex justify-content-center align-items-center">
+                <img class="img-fluid h-50" src="{{ Vite::asset('resources/img/boolean-logo.png') }}" alt="Logo">
+            </div>
+            <!-- /logo -->
+
+            <!-- icone -->
+            <div class="col d-flex justify-content-end align-items-center gap-2 fs-4">
+                <span>⌘</span>
+                <span>&hearts;</span>
+                <span>⇧</span>
+            </div>
+            <!-- /icone -->
+
+        </div>
+    </div>
 </header>
+
