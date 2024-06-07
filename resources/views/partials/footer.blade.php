1365ab45a7a34e18d0d2a69991ef92cc61a2f5d9
andrea-bertaglia <andreabertaglia@gmail.com>
2024-06-07 19:15:59 +0200

    feat: footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,33 @@
-<footer class="bg-success">
-    FOOTER
+<footer class="bg-black">
+    <div class="container text-white py-3">
+        <div class="row d-flex justify-content-between">
+            {{-- box di sinistra con links --}}
+            <div class="col-8">
+                <h6 class="fw-bold">Boolando s.r.l.</h6>
+                <div class="col d-flex align-items-center">
+                    <ul class="d-flex align-items-center mb-0 list-unstyled gap-3">
+                        <li><a class="text-decoration-none text-white" href="#"><small>Informazioni legali</small></a></li>
+                        <li><a class="text-decoration-none text-white" href="#"><small>Informativa sulla privacy</small></a></li>
+                        <li><a class="text-decoration-none text-white" href="#"><small>Diritto di recesso</small></a></li>
+                    </ul>
+                </div>
+            </div>
+            {{-- /box di sinistra con links --}}
+
+            {{-- box di destra con social --}}
+            <div class="col-3">
+                <h6 class="text-center fw-bold">
+                    Seguici anche su
+                </h6>
+                <div class="text-center">
+                    ◉ ◉ ◉ ◉ ◉ 
+                </div>
+            </div>
+            {{-- /box di destra con social --}}
+
+        </div>
+
+
+        
+    </div>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,29 +2,29 @@
     <div class="container">
         <div class="row text-white">
             
-            <!-- menù links -->
+            {{-- menù links --}}
             <div class="col fw-bold d-flex align-items-center">
                 <ul class="d-flex align-items-center mb-0 list-unstyled gap-3">
-                    <li>Donna</li>
-                    <li>Uomo</li>
-                    <li>Bambini</li>
+                    <li><a class="text-decoration-none text-white" href="#">Donna</a></li>
+                    <li><a class="text-decoration-none text-white" href="#">Uomo</a></li>
+                    <li><a class="text-decoration-none text-white" href="#">Bambini</a></li>
                 </ul>
             </div>
-            <!-- /menù links -->
+            {{-- /menù links --}}
 
-            <!-- logo -->
+            {{-- logo --}}
             <div class="col d-flex justify-content-center align-items-center">
                 <img class="img-fluid h-50" src="{{ Vite::asset('resources/img/boolean-logo.png') }}" alt="Logo">
             </div>
-            <!-- /logo -->
+            {{-- /logo --}}
 
-            <!-- icone -->
+            {{-- icone --}}
             <div class="col d-flex justify-content-end align-items-center gap-2 fs-4">
                 <span>⌘</span>
                 <span>&hearts;</span>
                 <span>⇧</span>
             </div>
-            <!-- /icone -->
+            {{-- /icone --}}
 
         </div>
     </div>
