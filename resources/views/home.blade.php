9bdc81f6eb40792d66b84b389f7979afd2e782b3
andrea-bertaglia <andreabertaglia@gmail.com>
2024-06-08 17:06:30 +0200

    feat: product card in partial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,23 +8,10 @@
     <div class="container py-5">
         <div class="row g-3">
             <div class="col-4">
-                {{-- card prodotto --}}
-                <div class="card-img position-relative">
-                    <img class="w-100" src="{{ Vite::asset('resources/img/1.webp') }}" alt="Testo">
-                    <span class="ms_like">&hearts;</span>
-                    <div class="ms_labels">
-                        <span class="ms_discount">-50%</span>
-                        <span class="ms_category">Sostenibilità</span>
-                    </div>
-                </div>
-                <div class="card-text d-flex flex-column">
-                    <span>Levi's</span>
-                    <span>Relaxed Fit</span>
-                    <p><span>14,99 €</span>29,99 €</p>
-                </div>
-            </div>
+            {{-- card prodotto --}}
+                @include('partials.product-card')
             {{-- /card prodotto --}}
-
+            </div>
         </div>
     </div>
 @endsection
---
+++ resources/views/partials/product-card.blade.php
@@ -0,0 +1,13 @@
+<div class="card-img position-relative">
+    <img class="w-100" src="{{ Vite::asset('resources/img/1.webp') }}" alt="Testo">
+    <span class="ms_like">&hearts;</span>
+    <div class="ms_labels">
+        <span class="ms_discount">-50%</span>
+        <span class="ms_category">Sostenibilità</span>
+    </div>
+</div>
+<div class="card-text d-flex flex-column">
+    <span>Levi's</span>
+    <span>RelaxedZ</span>
+    <p><span>14,99 €</span>29,99 €</p>
+</div>
