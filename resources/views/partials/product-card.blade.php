d5c39cbd36712074629eca03bd2317107f8c8049
andrea-bertaglia <andreabertaglia@gmail.com>
2024-06-09 12:11:22 +0200

    feat: calculate discount and print on page

--- resources/views/partials/product-card.blade.php
+++ resources/views/partials/product-card.blade.php
@@ -4,9 +4,13 @@
     <div class="ms_labels">
     {{-- badges (se presenti) --}}
     {{-- separo in due foreach per questioni di layout (per stampare prima lo sconto e poi il tag) --}}
-        @foreach ($product['badges'] as $badge)
+    @foreach ($product['badges'] as $badge)
+            {{-- aggiungo una variabile per memorizzare l'importo dello sconto --}}
+            <?php $amount_discount = 0 ?>
             @if($badge['type'] === 'discount')
                 <span class="ms_discount">{{$badge['value']}}</span>
+                    {{-- se è prsente il value discount, calcolo l'importo dello sconto (valore negativo) --}}
+                    <?php $amount_discount = round($product['price'] * intval($badge['value']) / 100, 2) ?>
             @endif
         @endforeach
         @foreach ($product['badges'] as $badge)
@@ -22,7 +26,7 @@
     <span class="small">{{$product['brand']}}</span>
     <span class="fw-bold fs-6">{{strtoupper($product['name'])}}</span>
     <div>
-        <span class="me-1 text-danger fw-bold fs-6">{{$product['price']}} €</span>
-        <span class="text-decoration-line-through">29,99 €</span>
+        <span v-if="{{$amount_discount !== 0}}" class="me-1 text-danger fw-bold fs-6">{{($product['price'] + $amount_discount )}} €</span>
+        <span class="text-decoration-line-through">{{$product['price']}} €</span>
     </div>
 </div>
